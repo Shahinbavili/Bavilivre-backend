9773719bb85d148a97bbc6ce63d12fd903cdd872
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2025-10-20 12:47:19 +0200

    Initialize project with Spring Initializr

---
+++ src/main/java/com/bavilivre/bavilivre_backend/BavilivreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bavilivre.bavilivre_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BavilivreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BavilivreBackendApplication.class, args);
+	}
+
+}
