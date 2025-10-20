c95381d458871216b55637545acc01381b054a9c
shahin <shahinbavili@gmail.com>
2025-10-20 13:46:53 +0200

    Add  a test to verify application context

--- src/test/java/com/bavilivre/bavilivre_backend/BavilivreBackendApplicationTests.java
+++ src/test/java/com/bavilivre/bavilivre_backend/BavilivreBackendApplicationTests.java
@@ -1,13 +1,26 @@
 package com.bavilivre.bavilivre_backend;
 
+import com.bavilivre.bavilivre_backend.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BavilivreBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    // Check that the Spring context is loading
+    @Test
+    void contextLoads() {
+    }
 
+    // Simple test: checks that SecurityConfig is loaded
+    @Autowired
+    private SecurityConfig securityConfig;
+
+    @Test
+    void securityConfigLoads() {
+        assertThat(securityConfig).isNotNull();
+    }
 }
